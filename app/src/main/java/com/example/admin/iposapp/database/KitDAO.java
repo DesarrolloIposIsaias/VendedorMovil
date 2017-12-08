0ffc0f650c248903c771cd00a8b195b653093133
josueben <josuebenbeatle@outlook.com>
2017-12-08 15:41:17 -0600

    Test

---
+++ app/src/main/java/com/example/admin/iposapp/database/KitDAO.java
@@ -0,0 +1,8 @@
+package com.example.admin.iposapp.database;
+
+/**
+ * Created by usuario on 08/12/2017.
+ */
+
+public class KitDAO {
+}
---
+++ app/src/main/java/com/example/admin/iposapp/model/Kit.java
@@ -0,0 +1,17 @@
+package com.example.admin.iposapp.model;
+
+import java.util.Date;
+
+/**
+ * Created by usuario on 08/12/2017.
+ */
+
+public class Kit {
+    private String producto;
+    private String parte;
+    private double cantidad;
+    private double costo;
+    private String id;
+    private Date idFecha;
+    private String idHora;
+}
